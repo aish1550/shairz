cffe9053c4110d0e0971b75d8498163af7edaf70
Aadam Ishmael <Aadam@crabclaw.co.uk>
2023-10-14 01:42:54 +0100

    exercise 1

--- src/index.js
+++ src/index.js
@@ -2,6 +2,22 @@ import express from "express";
 
 const app = express();
 
-app.use((req, res, next) => res.send("<h1>hello</h1>"));
+// app.use((req, res, next) => {
+//   console.warn("first middleware");
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   console.warn("second middleware");
+//   res.send("<h1>hello</h1>");
+// });
+
+app.use("/users", (req, res, next) => {
+  res.send(`<h1>Users</h1>`);
+});
+
+app.use("/", (req, res, next) => {
+  res.send(`<h1>Home</h1>`);
+});
 
 app.listen(3000);
